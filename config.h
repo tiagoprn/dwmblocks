c8847dd7f6f353f496080aab3a8c0a8ca4fd0cdb
tiago.paranhos <tiagoprn@gmail.com>
2021-01-06 11:48:56 -0300

    feat: updated info on bar.

--- config.h
+++ config.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22},
-	{"",	"sb-price btc Bitcoin 💰",				9000,	21},
+	{"", "sb-kbdleds", 0, 1},
+	{"", "sb-loadavg", 1, 1},
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	{"",	"sb-memory",	10,	14},
 	{"",	"sb-cpu",		10,	18},
 	{"",	"sb-moonphase",	18000,	17},
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	30,	1},
 	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	{"",	"sb-help-icon",	1,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
